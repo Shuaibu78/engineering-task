db3eb20a583857946a90815aa9b0958128f75ce6
shuaibu Muhammad <devshuaib@gmail.com>
2022-07-12 19:28:43 +0100

    implemented the search and filter functionality

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import LaunchesPastCard from "./components/card";
 import "./App.css";
 
 const GET_LAUNCHES_PAST = gql`
-  query launchesPast($limit: Int) {
-    launchesPast(limit: $limit) {
+  query launchesPast($limit: Int, $find: LaunchFind) {
+    launchesPast(limit: $limit, find: $find) {
       mission_name
       launch_date_local
       launch_site {
@@ -23,17 +23,25 @@ const GET_LAUNCHES_PAST = gql`
 `;
 
 function App() {
-  const [launchYear, setLaunchYear] = useState([]);
+  const [launchData, setLaunchData] = useState([]);
   const [siteName, setSiteName] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchParam] = useState(["mission_name", "launch_site"]);
   const { loading, error, data } = useQuery(GET_LAUNCHES_PAST, {
-    variables: { limit: 40, find: { site_name: `${siteName}` } },
+    variables: { limit: 40, find: { site_name: siteName } },
   });
 
   useEffect(() => {
-    setLaunchYear(data?.launchesPast);
-  }, []);
+    setLaunchData(data?.launchesPast);
+  }, [data]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="spinner-container">
+      <div className="loading-spinner">
+      </div>
+      <h4>Loading Launch Past Data</h4>
+    </div>
+  );
   if (error) return <p>Error</p>;
 
   const launchYears = [];
@@ -50,27 +58,39 @@ function App() {
   const specificSiteNames = new Set(siteNames);
   const pastLaunchesYear = [];
   const siteNamesList = [];
-  const rocketLaunchedYear = specificLaunchYears.forEach((year) => 
+  const rocketLaunchedYear = specificLaunchYears.forEach((year) =>
     pastLaunchesYear.push(year)
   );
   const rocketSiteName = specificSiteNames.forEach((site) =>
     siteNamesList.push(site)
   );
 
-  console.log(launchYear);
+  const handleSearch = () => {
+    return launchData?.filter((data) => (searchParam.some((newItem) => (
+      data[newItem]
+        .toString()
+        .toLowerCase()
+        .indexOf(searchQuery.toLowerCase()) > -1
+    ))
+    ));
+  }
+
   return (
     <div className="container">
-      <div>
-        <input type="text" />
+      <div className="searchContainer">
+        <input type="text" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
       </div>
-      <div>
-        <select onChange={(e) => setSiteName(e.target.value)}>
-          {
-            siteNamesList.map((site) => (
-              <option value={site}>{site}</option>
-            ))
-          }
-        </select>
+      <div className="selectContainer">
+        <label htmlFor="sortRockets">Filter By Site Name:</label>
+        <div className="select">
+          <select id="sortRockets" value={siteName} onChange={(e) => setSiteName(e.target.value)}>
+            <option value="">All site</option>
+            <option value="CCAFS SLC 40">CCAFS SLC 40</option>
+            <option value="VAFB SLC 4E">VAFB SLC 4E</option>
+            <option value="KSC LC 39A">KSC LC 39A</option>
+          </select>
+          <span className="focus"></span>
+        </div>
       </div>
       <div className="cardContainer">
         {pastLaunchesYear.map((year) => (
@@ -79,7 +99,7 @@ function App() {
               <h2>Launch Year:</h2>
               <h2>&ensp;{year}</h2>
             </div>
-            <LaunchesPastCard data={launchYear} launchedYear={year} />
+            <LaunchesPastCard launchedYear={year} handleSearch={handleSearch} />
           </div>
         ))}
       </div>
